f3a5e4ad127ee0bedbb32d9a07cceee86aaccda2
yasuko86 <59428496+yasuko86@users.noreply.github.com>
2020-04-07 19:51:55 +1000

    Add private routes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -16,8 +16,13 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-export { firebase, database as default }; 
+googleAuthProvider.setCustomParameters({
+  'prompt': 'select_account'
+})
+
+export { firebase, googleAuthProvider, database as default }; 
 
 // // child_removed
 // database.ref('expenses').on('child_removed', (snapshot) => {
